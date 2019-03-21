eec74b2faad2efd9a2b1ee89f41f8a070057c582
wangky1025 <373721041@qq.com>
2019-03-21 21:22:35 +0800

    哈哈

--- pattern-decotator/src/com/open/pattern/decotator/old/ISigninService.java
+++ pattern-decotator/src/com/open/pattern/decotator/old/ISigninService.java
@@ -5,6 +5,5 @@ import com.open.pattern.decotator.base.ResultMsg;
 public interface ISigninService {
 
     public ResultMsg regist(String username, String password);
-
     public ResultMsg login(String username,String password);
 }
